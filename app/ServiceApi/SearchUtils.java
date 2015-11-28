738e279e22c6f9c0ba41086ef72f7f8c4199f7a1
B Hamza <hamza.bbahassou@gmail.com>
2015-11-28 17:56:30 +0100

    HBA : fix bug du parsing result jackson to GlobalSearch<T>, cela en introduisant le TypeReference Jackson.

--- app/ServiceApi/SearchUtils.java
+++ app/ServiceApi/SearchUtils.java
@@ -1,7 +1,5 @@
 package ServiceApi;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -14,6 +12,7 @@ import javax.inject.Inject;
 import models.GlobalResults;
 import models.Repository;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,6 +22,10 @@ import play.libs.F.Promise;
 import play.libs.ws.WSClient;
 import play.libs.ws.WSResponse;
 
+/**
+ * @author hamza
+ *
+ */
 public abstract class SearchUtils {
 
 	final String url;
@@ -43,23 +46,49 @@ public abstract class SearchUtils {
 		return this;
 	}
 
+	
+	/**
+	 * getElementsJson fait appel à getElementsGeneric avec comme paramétre la class JsonNode pour créer un résultat de type JsonNode
+	 * @return JsonNode 
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
 	public JsonNode getElementsJson() throws MalformedURLException, IOException {
 
 		Logger.debug("Jsoncall of the searchUtils getElemntsJson ...");
-		JsonNode repo = this.getElemntsGeneric(JsonNode.class);
+		JsonNode repo = this.getElemntsGeneric(new TypeReference<JsonNode>() {});
 		return repo;
 
 	}
+
 	
+	/**
+	 * getElements fait appel à getElementsGeneric avec comme paramétre la class GlobalResults<Repository> pour créer un résultat de type GlobalResult<Repository> (liste de répertoire)
+	 * @return GlobalResults<Rpository> GlobalResults est un resultat générique de la recherche vu que 
+	 * 			sur l'api git fourni par github le resultat de search et tjr le même (user, repository...)
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
 	public GlobalResults<Repository> getElements() throws MalformedURLException, IOException {
 
 		Logger.debug("ObjectsRepo call of the searchUtils ...");
-		GlobalResults<Repository> repo = this.getElemntsGeneric(GlobalResults.class);
+		GlobalResults<Repository> repo = this.getElemntsGeneric(new TypeReference<GlobalResults<Repository>>() {});
 		return repo;
 
 	}
 	
-	public<T> T getElemntsGeneric(Class<T> genericClass) throws MalformedURLException, IOException{
+	
+	
+	/**
+	 * getElemntsGeneric est la méthode responsable d'apporter les résultat de l'api github en utulisant HttpUrlConnection enfin s'appuyant sur Jackson-core2.(x>2)
+	 * aufait, l'utilisation du TypeReference revient au problème d'utiliser un appel générique du mapper avec la class GlobalResults<Rpository>
+	 * et puis nous résoud le problème de l'unchecked conversion 
+	 * @param genericClass de type TypeReference<T>
+	 * @return Classe générique 
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
+	public<T> T getElemntsGeneric(TypeReference<T> genericClass) throws MalformedURLException, IOException{
 		T repo ;
 		try {
 			Logger.debug("Genericcall of the searchUtils getElemntsGeneric ...");
@@ -82,6 +111,7 @@ public abstract class SearchUtils {
 						+ conn.getResponseCode());
 			}
 			ObjectMapper objMapper = new ObjectMapper();
+			objMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 			repo = objMapper.readValue(conn.getInputStream(), genericClass);
 
 			conn.disconnect();
@@ -96,19 +126,20 @@ public abstract class SearchUtils {
 		}
 		return repo;
 	}
-//	public JsonNode getElementsJson() throws MalformedURLException, IOException{
-//		return Json.parse(this.getElementsResult().toString());
-//	}
 	
-//	public GlobalResults<Repository> getElements(){
-//		ObjectMapper objMapper = new ObjectMapper();
-//		objMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//		GlobalResults<Repository> repo = objMapper.readValue(this.getElementsJson()., GlobalResults.class);
-//	}
-	
+	/**
+	 * l'utilisation d'appels asynchrone !!! Non encore tester
+	 * @return promise response
+	 */
 	@Inject WSClient ws;
 	public Promise<WSResponse> getElementsPromise(){
-		Promise<WSResponse> promise = ws.url("https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc").get();
+		String appendParam = "";
+		if (!params.isEmpty()) {
+			appendParam = "?" + params.get(0);
+			for (int i = 1; i < params.size(); i++)
+				appendParam += "&" + params.get(i);
+		}
+		Promise<WSResponse> promise = ws.url(this.url + appendParam).get();
 		return promise;
 	}
 
